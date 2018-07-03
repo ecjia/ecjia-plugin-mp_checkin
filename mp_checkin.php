dff6b5f992b9d44ec2b6f515c0281f9ad1591200
东方皓 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-07-03 18:32:14 +0800

    更新公众平台插件版本号至v1.18.0

--- mp_checkin.php
+++ mp_checkin.php
@@ -49,7 +49,7 @@ Plugin Name: 签到
 Plugin URI: http://www.ecjia.com/plugins/ecjia.mp_checkin/
 Description: 使用插件可以让微信公众平台用户签到获取积分。
 Author: ECJIA TEAM
-Version: 1.0.0
+Version: 1.18.0
 Author URI: http://www.ecjia.com/
 Plugin App: platform
 */
