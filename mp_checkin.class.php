08a9046b71ad94bf8c1fd8a049104d6df36a97e8
张春晨 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-07-26 14:49:45 +0800

    积分签到

--- mp_checkin.class.php
+++ mp_checkin.class.php
@@ -148,7 +148,7 @@ class mp_checkin extends PlatformAbstract
                         'Title'         => '签到成功',
                         'Description'   => '获取1积分~~',
                         'Url'           => RC_Uri::url('platform/plugin/show', array('handle' => 'mp_checkin/init', 'openid' => $openid, 'uuid' => $uuid)),
-                        'PicUrl'        => RC_Plugin::plugin_dir_url(__FILE__) . '/images/wechat_thumb_pic.png',
+                        'PicUrl'        => RC_Plugin::plugin_dir_url(__FILE__) . '/images/wechat_thumb_pic_success.png',
                     );
                 } else {
                     $articles = array(
